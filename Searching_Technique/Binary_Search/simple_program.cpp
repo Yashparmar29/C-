50c60e29c471e45961f2ff595622e613adc078ba
yash parmar <parmaryas29@gmail.com>
2026-03-10 00:07:51 +0530

    add binary search

--- Searching_Technique/Binary_Search/simple_program.cpp
+++ Searching_Technique/Binary_Search/simple_program.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class method
 {
     int n;
-   // int arr[];
+    int arr[];
 
     public :
         method()
         {
             cout << "Enter no of element :- ";
             cin >> n;
-            int arr[n];
+            arr[n];
             for(int i=0;i<n;i++)
             {
                 cout << "Enter Element :-  ";
@@ -29,11 +29,45 @@ class method
             }
             for(int i=0;i<n;i++)
             {
-                cout << arr[i] << " , ";
+                cout << arr[i] << " , " ;
+                cout << endl;
+            }
+        }
+        void b_search()
+        {
+            cout << "value of search :- ";
+            int s_value;
+            cin >> s_value;
+
+            int head = n-1;
+            int lower = 0;
+
+            while(lower <= head)
+            {
+                int middle = (lower+head)/2;
+                if(arr[middle] == s_value)
+                {
+                    cout << "Value is in Array...";
+                    cout << "index no :- " << middle;
+                    break;
+                }
+                else if(arr[middle] < s_value)
+                {
+                    lower = middle;
+                }
+                else if(arr[middle] > s_value)
+                {
+                    head = middle;
+                }
+                else 
+                {
+                    cout << "value is not in Array..";
+                }
             }
         }
 };
 int main()
 {
     method m;
+    m.b_search();
 }
