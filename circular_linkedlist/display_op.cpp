9a32c37d9d48508724d9a020c722c97012914118
yash parmar <parmaryas29@gmail.com>
2026-04-24 11:25:35 +0530

    display_operation program

---
+++ circular_linkedlist/display_op.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+class method
+{
+    Node* last = NULL;
+    public :
+    void push(int value) 
+    {
+        Node* newNode = new Node();
+        newNode->data = value;
+
+        if (last == NULL) {
+            last = newNode;
+            last->next = last;
+        } else {
+            newNode->next = last->next;
+            last->next = newNode;
+        }
+    }
+    void display() 
+    {
+        if (last == NULL) 
+        {
+            cout << "List is empty\n";
+            return;
+        }
+
+        Node* temp = last->next;  
+        do {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        } 
+        while (temp != last->next);
+
+        cout << "(back to start)\n";
+    }
+};
+int main()
+{
+    method m;
+    m.push(28);
+    m.push(36);
+    m.push(67);
+
+    cout << "List: ";
+    m.display();
+
+}
